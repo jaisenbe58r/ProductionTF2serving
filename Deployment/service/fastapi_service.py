6407420c97ec8e2b5f1d7a0de3f974979f10a30a
Jaime Sendra <jsendra@autis.es>
2020-12-03 23:27:23 +0100

    add_middleware

--- Deployment/service/fastapi_service.py
+++ Deployment/service/fastapi_service.py
@@ -34,9 +34,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
-    # allow_credentials=True,
-    # allow_methods=["*"],
-    # allow_headers=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 def allowed_file(filename):
