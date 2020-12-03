dbcb29b86d562c92b78a470a0c2b086bae556d8b
Jaime Sendra <jsendra@autis.es>
2020-12-03 23:23:42 +0100

    UPLOAD_FOLDER

--- Deployment/service/fastapi_service.py
+++ Deployment/service/fastapi_service.py
@@ -15,7 +15,8 @@ import json
 import numpy as np
 from tensorflow.keras.preprocessing import image
 
-UPLOAD_FOLDER = 'uploads/images'
+# UPLOAD_FOLDER = 'uploads/images'
+UPLOAD_FOLDER = '~/Pebrassos-detection/Deployment/service/uploads/images'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 SEED = 99
